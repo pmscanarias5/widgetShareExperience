1a0c7812573108f2cfe63f8d5464fdb0cbe42827
Pedro Martin <pmscanarias5@gmail.com>
2026-03-30 10:59:08 +0200

    :boom: Primer commit

---
+++ src/runtime/style.ts
@@ -0,0 +1,29 @@
+import { type IMThemeVariables, css, type SerializedStyles } from 'jimu-core'
+
+export function getStyle (theme: IMThemeVariables,isInController:boolean): SerializedStyles {
+  // let theme = this.props.theme;
+  return css`
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    // if in controller, has padding
+    padding: ${isInController ? '8px' : '0'};
+    .items {
+      display: flex;
+    }
+    `
+}
+
+export function getPopupStyle (theme: IMThemeVariables): SerializedStyles {
+  // let theme = this.props.theme;
+  return css`
+    padding: 16px;
+    /*margin: 0.5rem;*/
+    min-width: 360px;
+    max-width: 380px;
+
+    .popup-item-buttons-wrapper {
+      margin: 16px -8px 0 -8px;
+    }
+    `
+}
